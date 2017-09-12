eb420177acf5b56ad1f22f9d211a0204d658ab8c
Samrat Bhattacharjee <samrat.bhattacharjee@capgemini.com>
2017-09-12 11:29:05 +0530

    Updated the prop file

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -20,7 +20,7 @@ public class App {
 
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-        SpringApplication.run(App.class, args); 
+        SpringApplication.run(App.class, args);
     }
 
     @Bean
